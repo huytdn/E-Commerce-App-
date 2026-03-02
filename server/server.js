7048d793a2cde342ed727e88c265352955d388b0
Huy Trần Đình Nhật <huytdn137@gmail.com>
2026-03-02 17:24:27 +0700

    get product , add product image upload with multer and cloudinary integration

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@ import connectDB from "./configs/db.js";
 import "dotenv/config";
 import userRoute from "./routes/userRoute.js";
 import sellerRoute from "./routes/sellerRoute.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import productRouter from "./routes/productRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB();
+await connectCloudinary();
 
 const allowedOrigins = ["http://localhost:5173"];
 
@@ -20,6 +23,7 @@ app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.get("/", (req, res) => res.send("API is Working"));
 app.use("/api/user", userRoute);
 app.use("/api/seller", sellerRoute);
+app.use("/api/product", productRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
